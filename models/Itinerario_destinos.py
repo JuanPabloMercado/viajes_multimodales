1583c82cc8e480ab120f2d995e454a1faade2c97
Juan Pablo <juanpablomercado93@gmail.com>
2025-09-26 15:45:16 -0300

    SIG-28 Se actualiza la arquitectura de la tabla Itinerario_destinos para corresponder a la estructura de las relaciones N:M con las tablas Itinerarios y Destinos_generales.

--- models/Itinerario_destinos.py
+++ models/Itinerario_destinos.py
@@ -4,9 +4,11 @@ class Itinerario_destinos(Base):
     __tablename__ = 'Itinerario_Destinos'
     
     #Definicion de la clave primaria compuesta
-    id_itinerarios = Column(Integer, ForeignKey(Itinerarios.id_itinerarios), primary_key=True, nullable=False)
-    id_destinos_generales = Column(Integer, ForeignKey(Destinos_Generales.id_destinos_generales), primary_key=True, nullable=False)
-    id_destinos_especificos = Column(Integer, ForeignKey(Destinos_Especificos.id_destinos_especificos), primary_key=True, nullable=False)
+    id_itinerarios = Column(Integer, ForeignKey('Itinerarios.id_itinerarios'), primary_key=True, nullable=False)
+    id_destinos_generales = Column(Integer, ForeignKey('Destinos_Generales.id_destinos_generales'), primary_key=True, nullable=False)
+
+    # Destino específico opcional
+    id_destinos_especificos = Column(Integer, ForeignKey('Destinos_Especificos.id_destinos_especificos'), nullable=True)
 
     #Relaciones hacia las tablas principales.
     itinerarios_relacion = relationship('Itinerarios', back_populates='itinerario_destinos_relacion')
