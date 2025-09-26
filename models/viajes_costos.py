9e02638bd7d287e00f2ea2cfc68b1bf0fa6a3e87
Juan Pablo <juanpablomercado93@gmail.com>
2025-09-26 14:25:44 -0300

    SIG-22 Se actualiza la arquitectura de la tabla Viajes_costos para estructurar de manera correcta las relaciones N:M cin las tablas Viajes y Costos_viajes.

--- models/viajes_costos.py
+++ models/viajes_costos.py
@@ -7,10 +7,7 @@ class Viajes_costos(Base):
     id_costos_viajes = Column(Integer, ForeignKey('costos_viajes.id_costos_viajes'), primary_key=True, nullable=False)
     id_viajes = Column(Integer, ForeignKey('viajes.id_viajes'), primary_key=True, nullable=False)
 
-    #Relaciones hacia las tablas principales.
-    viajes_relacion = relationship('Viajes', back_populates='viajes_costos_relacion')
-    costos_viajes_relacion = relationship('Costos_viajes', back_populates='viajes_costos_relacion')
-
+    
 
 
 
