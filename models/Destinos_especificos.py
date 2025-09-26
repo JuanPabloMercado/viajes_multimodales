7d6cfff22b1eb4bf15fec82a91ecc9a3db8dd8c5
Juan Pablo <juanpablomercado93@gmail.com>
2025-09-26 15:41:34 -0300

    SIG-29 Se modifica la arquitectura de la clase Destinos_especificos para adaptar la esctructura a lass relaciones N:M con las tablas Itinerarios y Destinos_generales.

--- models/Destinos_especificos.py
+++ models/Destinos_especificos.py
@@ -8,8 +8,8 @@ class Destinos_especificos(Base):
     hora_fin = Column(DateTime, nullable=False)
     
     #Definción de las claves foráneas
-    id_costos_destinos_especificos = (Integer, ForeignKey('Costos_destinos_especificos.id_costos_destinos_especificos'), nullable=False)
-    id_destinos_generales = (Integer, ForeignKey('Destinos_generales.id_destinos_generales'), nullable=False)
+    id_costos_destinos_especificos = Column(Integer, ForeignKey('Costos_destinos_especificos.id_costos_destinos_especificos'), nullable=False)
+    id_destinos_generales = Column(Integer, ForeignKey('Destinos_generales.id_destinos_generales'), nullable=False)
     
     #Relaciones hacia las tablas principales.
     costos_destinos_especificos_relacion = relationship('Costos_destinos_especificos', back_populates='destinos_especificos_relacion')
