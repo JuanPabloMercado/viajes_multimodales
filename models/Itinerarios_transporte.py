6d553835f7e550a72c8c02e79220bb5c3cd33ad7
Juan Pablo <juanpablomercado93@gmail.com>
2025-09-27 11:38:49 -0300

    SIG-24 Se corrige error de referencia en la definción d elas claves primarias compuestas.

--- models/Itinerarios_transporte.py
+++ models/Itinerarios_transporte.py
@@ -4,8 +4,8 @@ class Itinerarios_transporte(Base):
     __tablename__ = 'Itinerarios_Transporte'
 
     #Definicion de la clave primaria compuesta
-    id_itinerarios = Column(Integer, ForeignKey(Itinerarios.id_itinerarios), primary_key=True, nullable=False)
-    id_medios_transporte = Column(Integer, ForeignKey(Medios_Transporte.id_medios_transporte), primary_key=True, nullable=False)
+    id_itinerarios = Column(Integer, ForeignKey('Itinerarios.id_itinerarios'), primary_key=True, nullable=False)
+    id_medios_transporte = Column(Integer, ForeignKey('Medios_Transporte.id_medios_transporte'), primary_key=True, nullable=False)
     
     
 
